9a74ccc0f05b979ede9858c2ac38382e1e48742d
DeFaDeDe <defadede0826@gmail.com>
2025-05-27 14:57:46 +0800

    1. Initialize some project 2. added Icon to my site

---
+++ C++/Personal_Budget_Tracker.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void displayMenu() {
+    printf("Personal Budget Tracker\n");
+    printf("1. Add Income\n");
+    printf("2. Add Expense\n");
+    printf("3. View Summary\n");
+    printf("4. Exit\n");
+}
